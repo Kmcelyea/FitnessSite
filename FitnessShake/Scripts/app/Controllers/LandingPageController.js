384c4ff6d4c4eefdd34e557f51538eb03498095e
Kyle McElyea <kylem@phase2online.com>
2017-06-16 23:59:50 -0500

    Update to darker theme moving more toward prototype site

--- FitnessShake/Scripts/app/Controllers/LandingPageController.js
+++ FitnessShake/Scripts/app/Controllers/LandingPageController.js
@@ -1,12 +1,33 @@
-﻿var LandingPageController = function ($scope) {
+﻿var LandingPageController = function ($scope, $state) {
     $scope.models = {
-        brandName:'INSERT BRAND NAME'
+        brandName: 'INSERT BRAND NAME',
+        coc: 1, //container one color
+        ctc: 2 //container two color
+
     };
 
     $scope.navbarProperties = {
         isCollapsed: true
     };
+
+    $state.go('stateOne');
+
+    $scope.$on('$stateChangeStart',
+        function(event, toState, toParams, fromState, fromParams) {
+            if (toState.name === "stateOne") {
+                $scope.models.coc = 1; //Black
+                $scope.models.ctc = 2; //White
+            }
+            if (toState.name === "stateTwo") {
+                $scope.models.coc = 2; 
+                $scope.models.ctc = 4;
+            }
+            if (toState.name === "stateThree") {
+                $scope.models.coc = 2;
+                $scope.models.ctc = 1;
+            }
+        });
 }
 
 // The $inject property of every controller (and pretty much every other type of object in Angular) needs to be a string array equal to the controllers arguments, only as strings
-LandingPageController.$inject = ['$scope'];
+LandingPageController.$inject = ['$scope', '$state'];
